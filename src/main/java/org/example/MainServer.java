8d89011654c2414fbcf3f4ecd0ebd765ce2a4b5d
Павел Трифонов <p4veltr@gmail.com>
2022-09-16 23:13:36 +0300

    Initial commit

---
+++ src/main/java/org/example/MainServer.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MainServer {
+    public static void main(String[] args) {
+        int port = 12345;
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                Socket clientSocket = serverSocket.accept(); // ждем подключения
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                final int clientPort = clientSocket.getPort();
+                System.out.println("New connection accepted. Port: " + clientPort);
+
+                final String name = in.readLine();
+
+                out.println(String.format("Hi %s, your port is %d", name, clientPort));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
